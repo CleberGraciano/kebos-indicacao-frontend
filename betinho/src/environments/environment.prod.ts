4b79110a72059d31c406ee71659aca89da97b7d1
cleberGraciano <cleber_batista2010@hotmail.com>
2023-06-05 20:13:04 -0300

    Criação de Dockerfile e Docker-compose

--- betinho/src/environments/environment.prod.ts
+++ betinho/src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  version:'1.0',
+  // auth:'https://kebos-backend-java.herokuapp.com/api/auth/',
+  // api:'https://kebos-backend-java.herokuapp.com/api/',
+  auth:'http://89.116.214.72:8081/api/auth/',
+  api:'http://89.116.214.72:8081/api/',
+  refreshToken: '',
+  redirect: '?redirect_uri=',
+  OAUTH2_URL: 'oauth2/authorization/',
 };
