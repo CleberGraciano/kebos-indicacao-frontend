d2a4a7cc1aee91a6eac84343de2bc0114938f089
cleberGraciano <cleber_batista2010@hotmail.com>
2024-06-25 22:59:07 -0300

    teste

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
-const BASE_URL = 'http://89.116.214.72:8081';
+const BASE_URL = 'http://www.indicakebosapi.com:8081';
 const URL_LOCAL = 'http://localhost:4200';
 const REDIRECT_URL = `?redirect_uri=${URL_LOCAL}/login`;
 const OAUTH2_URL = `oauth2/authorization`;
@@ -6,8 +6,8 @@ const OAUTH2_URL = `oauth2/authorization`;
 export const environment = {
   production: true,
   version: '1.0',
-  auth: 'http://89.116.214.72:8081/api/auth/',
-  api: 'http://89.116.214.72:8081/api/',
+  auth: 'http://www.indicakebosapi.com:8081/api/auth/',
+  api: 'http://www.indicakebosapi.com:8081/api/',
   refreshToken: '',
   GOOGLE_AUTH_URL: `${BASE_URL}/${OAUTH2_URL}/google/${REDIRECT_URL}`,
   FACEBOOK_AUTH_URL: `${BASE_URL}/${OAUTH2_URL}/facebook/${REDIRECT_URL}`,
